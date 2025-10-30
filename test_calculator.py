58b918670aad748820505553c5fb0271afd63517
NoNameSpirit <shirokov_petya56@mail.ru>
2025-10-30 19:00:18 +0300

    Black fromatting

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,15 @@
 from calculator import add, multiply, subtract
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
 
+
 def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(0, 5) == 0
+
 
 def test_subtract():
     assert subtract(10, 4) == 6
